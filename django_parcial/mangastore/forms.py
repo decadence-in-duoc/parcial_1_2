ef74a917eefff2b89226334024678f895c00a4c2
Veruwu <102052457+Veruwu@users.noreply.github.com>
2023-06-22 05:07:18 -0400

    Delete django_parcial directory

--- django_parcial/mangastore/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from .models import Producto
-
-
-class ProductoForm(ModelForm):
-    class Meta:
-        model = Producto
-        fields = ['nombre','categoria','descripcion','precio']
